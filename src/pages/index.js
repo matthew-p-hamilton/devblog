07d942dad038c995184b59aa9f20dd5321b86fd1
@hamilton3 <hamilton@getlinus.io>
2020-11-29 11:25:20 -0500

    update folder structure 2

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react"
 import { Link, graphql } from "gatsby"
 
-import Bio from "../components/bio"
+import Bio from "../components/bio/Bio"
 import Layout from "../layout"
 import SEO from "../components/seo/seo"
 
